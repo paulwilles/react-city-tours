1cbf4f33085642ec24d27d9e41aec9b881cd566b
pdjw01 <pdjw01@gmail.com>
2020-08-04 15:33:10 +1000

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Navbar.scss';
+import logo from '../../logo.png'
+function Navbar(props) {
+  return (
+    <nav className='navbar'>
+      <img src={logo} alt='City Tours Logo' />
+      <ul className='nav-links'>
+      <li>
+          <a href='/' className='nav-link'>
+            home
+          </a>
+        </li>
+        <li>
+          <a href='/' className='nav-link'>
+            about
+          </a>
+        </li>
+        <li>
+          <a href='/' className='nav-link active'>
+            tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
